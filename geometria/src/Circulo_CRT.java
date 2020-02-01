6954aa49503132056b7f3e1bb2fc7cfa5e9b6b2a
Celia <Celia@Celia-Portatil>
2020-02-01 22:34:49 +0100

    versión refactorizada

--- geometria/src/Circulo_CRT.java
+++ geometria/src/Circulo_CRT.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_CRT extends FiguraGeometrica_CRT {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_CRT(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
