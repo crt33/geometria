e69a80a5651afcfc6dfd7c97d8c2af10cb9e7cd9
Celia <Celia@Celia-Portatil>
2020-02-01 22:38:14 +0100

    método en una línea

--- geometria/src/Triangulo_CRT.java
+++ geometria/src/Triangulo_CRT.java
@@ -19,7 +19,7 @@ public class Triangulo_CRT extends FiguraGeometrica_CRT {
 	@Override
 	public double area() { //Utiliza la formula de Her—n
 		double sp;
-		sp = this.semiPerimetro();
+		sp = this.perimetro() / 2;
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
 	}
 	
